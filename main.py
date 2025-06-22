8412d5d6e908140d50abe331e0bed92750de1c9c
Bhavyansh Shivakoti <dabhavking@gmail.com>
2025-06-22 16:41:31 -0400

    first net edition

---
+++ main.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class CompensatorNet(nn.Module):
+    def __init__(self, input_dim):
+        super(CompensatorNet, self).__init__()
+
+        self.net = nn.Sequential(
+            nn.Linear(input_dim, 256),
+            nn.BatchNorm1d(256),
+            nn.GELU(),
+
+            nn.Linear(256, 512),
+            nn.BatchNorm1d(512),
+            nn.GELU(),
+
+            nn.Linear(512, 1024),
+            nn.BatchNorm1d(1024),
+            nn.GELU(),
+
+            nn.Linear(1024, 1024),
+            nn.Dropout(0.3),
+            nn.GELU(),
+
+            nn.Linear(1024, 512),
+            nn.BatchNorm1d(512),
+            nn.GELU(),
+
+            nn.Linear(512, 256),
+            nn.BatchNorm1d(256),
+            nn.GELU(),
+
+            nn.Linear(256, input_dim)  
+        )
+
+    def forward(self, x):
+        return self.net(x)
